64f715813ef6f46159f37c6bebbeb030e6688343
Karamdeep <karamdeep.singh11@gmail.com>
2020-02-13 05:41:30 +0530

    trees

---
+++ trees/bst/bsTrees.java
@@ -0,0 +1,189 @@
+
+
+import java.util.*;
+
+
+public class bsTrees {
+
+    static class Node {
+        /* 
+        This class is used for creation of each node of the bst.
+        */
+        int value;
+        Node left,right;
+
+        Node(int val) {
+            value = val;
+            left = right = null;
+        }
+    }
+
+    static Node root;
+
+    bsTrees() {
+        root = null;
+    }
+
+    //find minimum value of a given tree
+    static int minVal(Node head) {
+        
+        if(head.left != null) {
+            return minVal(head.left);
+        }
+        else {
+            return head.value;
+        }
+         
+    }
+
+    //recursive operation to set value in bst.
+    static Node addAndUpdate(Node head, int value) {
+        
+        if(head == null) {
+            return new Node(value);
+        }
+
+        if(head.value == value) {
+            return head;
+        }
+
+        if(value < head.value) {
+            head.left = addAndUpdate(head.left,value);
+        }
+
+        if(value > head.value) {
+            head.right = addAndUpdate(head.right,value);
+        }
+
+        return head;
+    }
+
+    //recursive operation to update value in bst.
+    static Node updateOld(Node head,int value, int newValue) {
+        
+        if(head ==  null) {
+            return head; 
+        }
+
+        if(head.value == value) {
+            head.value = newValue;
+            return head;
+        }
+
+        if(value < head.value) {
+            head.left = updateOld(head.left,value,newValue);
+        }
+
+        if(value > head.value) {
+            head.right = updateOld(head.right,value,newValue);
+        }
+
+        return head;
+    }
+
+    //recursive operation to delete value in bst.
+    static Node deleteRec(Node head,int value) {
+        
+        if(head ==  null) {
+            return head; 
+        }
+        
+        if(value < head.value) {
+            head.left = deleteRec(head.left,value);
+        }
+
+        if(value > head.value) {
+            head.right = deleteRec(head.right,value);
+        }
+
+        else {
+            if(head.right == null ) {
+                return head.left;
+            }
+
+            if(head.left == null ) {
+                return head.right;
+            }
+
+            else {
+                head.value = minVal(head.right);
+                deleteRec(head.right,head.value);
+            }
+        }
+        return head;
+    }
+
+//Q1  kth min sum
+    static int sum = 0;
+    static int counter = 0;
+    static int kthMinSumVal(Node head , int k) {
+        if(head == null) {
+            return -1 ;
+        }
+
+        kthMinSumVal(head.left,k);
+
+        counter++;
+
+        if(counter<=k) {
+            sum = sum + head.value;
+        }
+        else{
+            return sum;
+        }
+
+        kthMinSumVal(head.right,k);
+          
+        return sum;     
+    }
+    public static void kthMinSum(int value) {
+        System.out.println(kthMinSumVal(root,value));
+    }
+    
+//Q2    Vertical order traversal
+    {
+        class Values {
+            int min;
+            int max;
+        }
+
+        
+
+
+    }
+
+    //CRUD operations
+
+    //adding to the structure
+    static void add(int value) {
+        root = addAndUpdate(root,value);
+    }
+
+
+    //updating from the structure
+    static void update(int value , int newValue) {
+        root = updateOld(root,value,newValue);
+    }
+
+    //delete from the structure
+    static void delete(int value) {
+        root = deleteRec(root,value);
+    }
+
+    
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        bsTrees bst = new bsTrees();
+        for(int i=0;i<n;i++) {
+            bst.add(sc.nextInt());
+        }
+
+        int kthSmallestSum = sc.nextInt();
+
+        kthMinSum(kthSmallestSum);
+        
+        
+    }
+}
